6eca146ae70a80eb429e06df626c10fe4ce241d7
Felix-Antoine Laprise-Lemay <1231016@cstj.qc.ca>
2026-08-21 18:10:37 -0400

    correction animation

--- lapriselemay_solution#1/QuickLauncher/Constants.cs
+++ lapriselemay_solution#1/QuickLauncher/Constants.cs
@@ -6,7 +6,7 @@ namespace QuickLauncher;
 public static class Constants
 {
     public const string AppName = "QuickLauncher";
-    public const string Version = "1.2.2";
+    public const string Version = "1.2.3";
     
     /// <summary>Repo GitHub servant de source pour les mises à jour automatiques (Velopack).</summary>
     public const string UpdateRepositoryUrl = "https://github.com/1231016Felix/lapriselemay_personalRepo";
